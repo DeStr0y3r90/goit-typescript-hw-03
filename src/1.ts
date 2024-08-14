c59ea18d96d2e640a35a72e5ec2518ce331bba54
DeStr0y3r90 <98543747+DeStr0y3r90@users.noreply.github.com>
2024-08-14 18:04:54 +0100

    Initial commit

---
+++ src/1.ts
@@ -0,0 +1,15 @@
+/*
+  Клас Student, який містить три властивості: name, age та grade. 
+  Замість того, щоб оголошувати ці властивості в тілі класу, потім у конструкторі, і нарешті надавати їм значення, 
+  напишіть скорочену ініціалізацію.
+*/
+
+class Student {
+  constructor(
+    private name: string,
+    private age: number,
+    public grade: string
+  ) {}
+}
+
+export {};
